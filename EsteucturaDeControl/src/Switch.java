75d106e4bf7fa89562654e66e996a27ad02995be
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2024-10-11 11:15:48 +0200

    o

--- EsteucturaDeControl/src/Switch.java
+++ EsteucturaDeControl/src/Switch.java
@@ -6,7 +6,14 @@ public class Switch {
         //ejercicio2();
         //ejercicio3();
         //ejercicio4();
-        ejercicio5();
+        //ejercicio5();
+        //ejercicio6();
+        //ejercicio7();
+        //ejercicio8();
+        //ejercicio9();
+        //ejercicio10();
+
+
 
     }
     public static void ejercicio1(){
@@ -160,6 +167,125 @@ public class Switch {
                 break;
             case 12:
                 System.out.println("Es diciembre");
+                break;
+        }
+    }
+    public static void ejercicio6(){
+        System.out.println("Introduce un numero de 1 al 4");
+        int numero = lectorTeclado.nextInt();
+
+        switch (numero){
+            case 1:
+                System.out.println("Opcion 1 seleccionada:");
+                break;
+            case 2:
+                System.out.println("Opcion 2 seleccionada:");
+                break;
+            case 3:
+                System.out.println("Opcion 3 seleccionada:");
+                break;
+            case 4:
+                System.out.println("Opcion 4 seleccionada:");
+                break;
+        }
+    }
+    public static void ejercicio7(){
+        System.out.println("Introduce una nota del 1 al 5:");
+        int nota = lectorTeclado.nextInt();
+
+        switch (nota){
+            case 1:
+                System.out.println("Suspenso");
+                break;
+            case 2:
+                System.out.println("Aprobado");
+                break;
+            case 3:
+                System.out.println("Notable");
+                break;
+            case 4:
+                System.out.println("Sobresaliente");
+                break;
+            case 5:
+                System.out.println("Genial");
+                break;
+        }
+    }
+    public static void ejercicio8(){
+        System.out.println("Introduce un simbolo de operacion:");
+        char simbolo = lectorTeclado.next().charAt(0);
+        System.out.println("Introduce primer numero:");
+        int numero1 = lectorTeclado.nextInt();
+        System.out.println("Introduce segundo numero:");
+        int numero2 = lectorTeclado.nextInt();
+        float variable;
+
+        switch (simbolo){
+            case '+':
+                variable = numero1 + numero2;
+                System.out.println(variable);
+                break;
+            case '-':
+                variable = numero1 - numero2;
+                System.out.println(variable);
+                break;
+            case '*':
+                variable = numero1 * numero2;
+                System.out.println(variable);
+                break;
+            case '/':
+                variable = numero1 / numero2;
+                System.out.println(variable);
+                break;
+        }
+
+    }
+    public static void ejercicio9(){
+        System.out.println("Introduce un numero de 1 al 12");
+        int numero = lectorTeclado.nextInt();
+
+        switch (numero){
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Es invirno");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Es primavera");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Es verano");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Es otoño");
+                break;
+        }
+    }
+    public static void ejercicio10(){
+        System.out.println("Introduce el codigo:");
+        int codigo = lectorTeclado.nextInt();
+
+        switch (codigo){
+            case 1:
+                System.out.println("El precio de Iphone 14 pro es 1000 euros");
+                break;
+            case 2:
+                System.out.println("El precio de Poco m4 pro es 450 euros");
+                break;
+            case 3:
+                System.out.println("El precio de Iphone 15 pro es 1500 euros");
+                break;
+            case 4:
+                System.out.println("El precio de Iphone 16 pro es 1600 euros");
+                break;
+            case 5:
+                System.out.println("El precio de Iphone 11 pro es 800 euros");
                 break;
         }
     }
---
+++ ejercicios for/src/for1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class for1 {
+    static Scanner lectorTeclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        //ejercicio0();
+        ejercicio1();
+        //ejercicio2();
+        //ejercicio3();
+        //ejercicio4();
+        //ejercicio5();
+        //ejercicio6();
+        //ejercicio7();
+        //ejercicio8();
+        //ejercicio9();
+        //ejercicio10();
+        //ejercicio11();
+        //ejercicio12();
+        //ejercicio13();
+        //ejercicio14();
+        //ejercicio15();
+        //ejercicio16();
+    }
+    public static void ejercicio0(){
+        System.out.println("Introduce un numero:");
+        int numero = lectorTeclado.nextInt();
+        for (int i=0; i<11; i++){
+            System.out.printf("%d * %d = %d\n", numero,i,numero*i);
+        }
+    }
+    public static void ejercicio1(){
+        
+    }
+    public static void ejercicio2(){}
+    public static void ejercicio3(){}
+    public static void ejercicio4(){}
+    public static void ejercicio5(){}
+    public static void ejercicio6(){}
+    public static void ejercicio7(){}
+    public static void ejercicio8(){}
+    public static void ejercicio9(){}
+    public static void ejercicio10(){}
+    public static void ejercicio11(){}
+    public static void ejercicio12(){}
+    public static void ejercicio13(){}
+    public static void ejercicio14(){}
+    public static void ejercicio15(){}
+    public static void ejercicio16(){}
+}
