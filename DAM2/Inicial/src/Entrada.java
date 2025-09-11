06fdc19f70993565e773fc9eb564ea615eabf6a8
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2025-09-11 18:39:43 +0200

    ю

---
+++ DAM2/Inicial/src/Entrada.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Entrada {
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        System.out.println("Primer programa de repaso");
+
+
+
+
+    }
+
+    public void if_1() {
+        System.out.println("Introduce primer numero: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Introduce segundo numero: ");
+        int num2 = 
+    }
+}
