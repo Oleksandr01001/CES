b13279c270ababa21e339554994bf961d8f1eef1
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2025-09-19 08:05:41 +0200

    update

---
+++ DAM2_Pro/Tienda/src/Main.kt
@@ -0,0 +1,28 @@
+import model.Producto
+
+fun main() {
+
+    var camiseta: Producto = Producto(id = 1, precio = 14.99)
+    var zapatillas: Producto = Producto(2,20.0)
+    var pantalones = Producto(3,30.0,"Pantalones")
+    var gorra = Producto(4,20.0, descripcion = "Gorra molona")
+    var cartera = Producto(5, 5.0, "Cartera", "Catrera...")
+
+
+
+    val listaProductos: Array<Producto?> = arrayOf(camiseta, zapatillas, pantalones, gorra, cartera)
+
+    listaProductos[0]?.mostrarDatos()
+    listaProductos.last()?.precio=20.0
+    listaProductos[listaProductos.size-1]?.mostrarDatos()
+    listaProductos[2] = null
+
+
+    for (i in listaProductos) {
+        i?.mostrarDatos()
+    }
+
+
+
+
+}
---
+++ DAM2_Pro/Tienda/src/model/Producto.kt
@@ -0,0 +1,34 @@
+package model
+
+class Producto(var id: Int, var precio: Double=10.0,var nombre: String?=null, var descripcion : String?=null ) {
+
+
+
+
+    /*
+    constructor(id: Int, precio: Double, nombre: String): this(id,precio) {
+        this.nombre = nombre
+    }
+
+    constructor(id: Int, precio: Double, descripcion: String?): this(id, precio) {
+        this.descripcion = descripcion
+    }
+
+    constructor(id: Int, precio: Double, nombre: String, descripcion: String?): this(id, precio) {
+        this.nombre = nombre
+        this.descripcion = descripcion
+    }
+     */
+
+    fun mostrarDatos() {
+        println("ID: $id")
+        println("Precio: $precio")
+        println("Nombre: ${nombre ?: "SIN DEFINIR"}")
+        println("Descripcion: ${descripcion ?: "SIN DEFINIR"}")
+        println()
+    }
+
+
+
+
+}
