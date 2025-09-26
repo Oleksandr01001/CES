40d65c8094b7e7382f3d5303ef9f1720d48f9733
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2025-09-26 11:53:11 +0200

    T

--- DAM2_Pro/DI/Tema_1/TodoList/src/Main.java
+++ DAM2_Pro/DI/Tema_1/TodoList/src/Main.java
@@ -1,6 +1,7 @@
-import model.Encargo;
-import model.Persona;
-import model.Tarea;
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
 
@@ -12,7 +13,7 @@ public class Main {
         // tarea.asignarResponsable(new Persona("123A","Nombre1","Apellido1",23));
         // tarea.asignarResponsable(new Persona("123A","Nombre1","Apellido1",23));
         // tarea.asignarResponsable(new Persona("123A","Nombre1","Apellido1",23));
-        tarea.agregarEncargo(new Encargo(1,"Planificar la entrega"));
+        /*tarea.agregarEncargo(new Encargo(1,"Planificar la entrega"));
         tarea.agregarEncargo(new Encargo(2,"Codiicar la practica"));
         tarea.agregarEncargo(new Encargo(3,"Grabar video"));
         tarea.agregarEncargo(new Encargo(4,"Entregar practica"));
@@ -20,13 +21,26 @@ public class Main {
         tarea.completarEncargo(2);
         tarea.completarEncargo(3);
         tarea.completarEncargo(4);
+
+         */
         // tarea.listarEncargos();
-        tarea.completarTarea();
+        //tarea.completarTarea();
         // tarea.buscarEncargoId(9);
         // tarea.listarEncargosCompletados();
 
 
+        TareaProfesional tareaProfesional = new TareaProfesional("TareaP1","descripcion",
+                true,5,10000,new Date());
+        TareaPersonal tareaPersonal = new TareaPersonal("TareaP2","Descripcion",
+                true,5,"Alcorcon");
 
+        ArrayList<Tarea> tareas = new ArrayList<>();
+        tareas.add(tareaProfesional);
+        tareas.add(tareaPersonal);
+        for (Tarea tarea1 : tareas) {
+            System.out.println(tarea1);
+
+        }
 
     }
 }
