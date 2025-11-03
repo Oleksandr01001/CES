e09ee0b8c72a6491fb9c300808adcebd00539466
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2025-11-03 22:37:29 +0100

    update

---
+++ DAM2_Pro/DI/Tema_2/Calculadora/src/main/java/com/example/calculadora/controller/CalculadoraController.java
@@ -0,0 +1,100 @@
+package com.example.calculadora.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.lang.classfile.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalculadoraController implements Initializable {
+
+
+
+    @FXML
+    private Button BotonCos;
+
+    @FXML
+    private Button BotonEntre;
+
+    @FXML
+    private Button BotonIgual;
+
+    @FXML
+    private Button BotonMas;
+
+    @FXML
+    private Button BotonMenos;
+
+    @FXML
+    private Button BotonNum0;
+
+    @FXML
+    private Button BotonNum1;
+
+    @FXML
+    private Button BotonNum2;
+
+    @FXML
+    private Button BotonNum3;
+
+    @FXML
+    private Button BotonNum5;
+
+    @FXML
+    private Button BotonNum6;
+
+    @FXML
+    private Button BotonNum7;
+
+    @FXML
+    private Button BotonNum8;
+
+    @FXML
+    private Button BotonNum9;
+
+    @FXML
+    private Button BotonPor;
+
+    @FXML
+    private Button BotonRaiz;
+
+    @FXML
+    private Button BotonRecet;
+
+    @FXML
+    private Button BotonSin;
+
+    @FXML
+    private Button BotonTan;
+
+    @FXML
+    private Label LabelProceso;
+
+    @FXML
+    private Label LabelSum;
+
+    @FXML
+    private RadioButton RadioB;
+
+    @FXML
+    private RadioButton RadioC;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        acciones();
+
+    }
+
+    private void acciones() {
+
+    }
+
+
+}
---
+++ DAM2_Pro/DI/Tema_2/Calculadora/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.calculadora {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+
+    opens com.example.calculadora to javafx.fxml;
+    exports com.example.calculadora;
+}
