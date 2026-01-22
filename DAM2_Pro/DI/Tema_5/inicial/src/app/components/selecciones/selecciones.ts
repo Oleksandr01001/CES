b79a9a64f9eee92eb4bca66f390cc5cc97950bdb
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2026-01-22 07:53:50 +0100

    update

--- DAM2_Pro/DI/Tema_5/inicial/src/app/components/selecciones/selecciones.ts
+++ DAM2_Pro/DI/Tema_5/inicial/src/app/components/selecciones/selecciones.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-selecciones',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './selecciones.css',
 })
 export class Selecciones {
-
+  seleccion = '1';
+  tipoPago = '1';
 }
