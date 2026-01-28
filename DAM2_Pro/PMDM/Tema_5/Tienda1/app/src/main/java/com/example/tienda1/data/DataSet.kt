f73711b348923c73e83992da2a9d366d7c61c313
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2026-01-28 20:48:31 +0100

    update

---
+++ DAM2_Pro/PMDM/Tema_5/Tienda1/app/src/main/java/com/example/tienda1/data/DataSet.kt
@@ -0,0 +1,4 @@
+package com.example.tienda1.data
+
+class DataSet {
+}
---
+++ DAM2_Pro/PMDM/Tema_5/Tienda1/app/src/main/java/com/example/tienda1/ui/fragment/LoginFragment.kt
@@ -0,0 +1,40 @@
+package com.example.tienda.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tienda1.R
+import com.example.tienda1.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+        binding.btnRegistro.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+}
